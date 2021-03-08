9b6eb9736739f2952d589ca51a50c7ebb3d63f6a
Henry Vaughn <hcvaughn@unca.edu>
2021-03-08 17:53:16 -0500

    Finished chapter 1 of the globe_bank tutorial. Next is chapter 2: Building Web Pages with PHP

--- public/staff/index.php
+++ public/staff/index.php
@@ -1,12 +1,9 @@
-<!doctype html>
+<?php require_once('../../private/initialize.php'); ?>
 
-<html lang="en">
-  <head>
-    <title></title>
-    <meta charset="utf-8">
-  </head>
-
-  <body>
-
-  </body>
-</html>
+<?php $page_title = 'Staff Menu'; ?>
+<?php include(SHARED_PATH . '/staff_header.php'); ?>
+      
+<div id="content">
+</div>  
+    
+<?php include(SHARED_PATH . '/staff_footer.php'); ?>    
