c5d094fee19577cd62e7866ccccb813e17b4b182
Henry Vaughn <hcvaughn@unca.edu>
2021-04-05 16:38:33 -0400

    Completed chapter 8. Next up is the Challenge.

--- private/functions.php
+++ private/functions.php
@@ -1,4 +1,4 @@
-<?php
+<<?php
 
 function url_for($script_path) {
   // add the leading '/' if not present
@@ -9,30 +9,30 @@ function url_for($script_path) {
 }
 
 function u($string="") {
-    return urlencode($string);
+  return urlencode($string);
 }
 
 function raw_u($string="") {
-    return rawurlencode($string);
+  return rawurlencode($string);
 }
 
 function h($string="") {
-    return htmlspecialchars($string);
+  return htmlspecialchars($string);
 }
 
 function error_404() {
-    header($_SERVER["SERVER_PROTOCOL"] . "404 Not Found");
-    exit();
+  header($_SERVER["SERVER_PROTOCOL"] . " 404 Not Found");
+  exit();
 }
 
 function error_500() {
-    header($_SERVER["SERVER_PROTOCOL"] . "500 Internal Server Error");
-    exit();
+  header($_SERVER["SERVER_PROTOCOL"] . " 500 Internal Server Error");
+  exit();
 }
 
 function redirect_to($location) {
-    header("Location: " . $location);
-    exit;
+  header("Location: " . $location);
+  exit;
 }
 
 function is_post_request() {
@@ -41,6 +41,21 @@ function is_post_request() {
 
 function is_get_request() {
   return $_SERVER['REQUEST_METHOD'] == 'GET';
+}
+
+function display_errors($errors=array()) {
+  $output = '';
+  if(!empty($errors)) {
+    $output .= "<div class=\"errors\">";
+    $output .= "Please fix the following errors:";
+    $output .= "<ul>";
+    foreach($errors as $error) {
+      $output .= "<li>" . h($error) . "</li>";
+    }
+    $output .= "</ul>";
+    $output .= "</div>";
+  }
+  return $output;
 }
 
 ?>
